e714b27c0993b46405d3886062229236431c7f4e
ALAM-97 <alexamara97@gmail.com>
2021-08-30 19:03:22 +0200

    JS Function Added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
+// ------ seleziono i btn ------
+var generateBtn = document.getElementById("gen-btn");
+var deleteBtn = document.getElementById("del-btn");
+var ticketPrice;
+   
+// ------ inizializzo la funzione al click del btn genera ------
+generateBtn.addEventListener('click',
+    function() {
+
+        // ------ salvo gli user input ------
+        var fullName = document.getElementById("name").value;
+        var km = parseInt(document.getElementById("km").value);
+        var userAge = document.getElementById("age").value;
+
+        // ------ calcolo il prezzo del biglietto ------
+        if (userAge == "underage") {
+            ticketPrice = (km * 0.21 * 0.80);
+            ticketPrice = ticketPrice.toFixed(2);
+        } else if (userAge == "over") {
+            ticketPrice = (km * 0.21 * 0.60);
+            ticketPrice = ticketPrice.toFixed(2);
+        } else {
+            ticketPrice = (km * 0.21);
+            ticketPrice = ticketPrice.toFixed(2);
+        }
+        
+        // ------ stampo le informazioni del biglietto ------
+        document.getElementById("pax-name").innerHTML = fullName;
+
+    }
+)
+
+
+
 
